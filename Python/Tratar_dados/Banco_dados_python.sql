424422aa95d67f30db5bd2310b10012da67fab67
Mário Guimarães <mariorochedo329@gmail.com>
2024-09-21 18:56:19 -0300

    Update Banco_dados_python.sql

--- Python/Tratar_dados/Banco_dados_python.sql
+++ Python/Tratar_dados/Banco_dados_python.sql
@@ -11,10 +11,6 @@ CREATE TABLE dados(
     telefone VARCHAR(100),
     cidade VARCHAR(100)
 );
-DROP TABLE dados;
-SELECT
-	dados.nome,
-	COUNT(nome)
-    FROM dados GROUP BY nome;
+
 SELECT * FROM dados;
 SELECT * FROM dados WHERE sexo = 'F';
